6ca95142a1d6b25dd53163b03b7a6507341f6499
Javier Rodriguez <javicerodriguez@gmail.com>
2023-12-27 02:08:15 -0300

    dia 4

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -8,12 +8,25 @@ const List = () => {
     "Alfajores 🍪",
   ]);
 
+  const handleDelete = (index: number) => {
+    setGifts((prevGifts) => prevGifts.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
       <AddGift setGifts={setGifts} />
       <ul>
         {gifts.map((gift, index) => (
-          <li key={index}>{gift}</li>
+          <li key={index} className="flex gap-4 justify-between py-1">
+            {gift}
+            <button
+              className="bg-red-500 px-2 rounded-xl text-white text-xs"
+              type="button"
+              onClick={() => handleDelete(index)}
+            >
+              X
+            </button>
+          </li>
         ))}
       </ul>
     </div>
