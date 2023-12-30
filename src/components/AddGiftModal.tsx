c5dc4edeeb1242aa0176190c97e2b99d4e4a51a3
Javier Rodriguez <javicerodriguez@gmail.com>
2023-12-29 23:52:34 -0300

    dia 14 (wip)

--- src/components/AddGiftModal.tsx
+++ src/components/AddGiftModal.tsx
@@ -29,7 +29,10 @@ const AddGiftModal = ({
     </Dialog.Trigger>
     <Dialog.Portal>
       <Dialog.Overlay className="bg-slate-900 opacity-50 data-[state=open]:animate-overlayShow fixed inset-0" />
-      <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+      <Dialog.Content
+        onEscapeKeyDown={onClose}
+        className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none"
+      >
         <Dialog.Title className="m-0 text-[17px] font-medium mb-4">
           Agrega un regalo a la lista 🎁 🎁
         </Dialog.Title>
